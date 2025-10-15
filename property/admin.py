1e2f4119347d0fec76e0389a3be817c67114e986
Lev <leon.fissenko@gmail.com>
2025-10-15 22:38:25 +0500

    add Owner model and added setup to the admin UI

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -33,5 +33,15 @@ class ComplaintAdmin(admin.ModelAdmin):
     # readonly_fields = ['text']
 
 
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = [
+        'full_name',
+        'phonenumber',
+        'pure_phone'
+    ]
+    raw_id_fields = ['flats']
+
+
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin)
--- property/models.py
+++ property/models.py
@@ -82,3 +82,19 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'{self.user}, {self.flat}, {self.text}'
+
+
+class Owner(models.Model):
+    full_name = models.CharField('ФИО владельца', max_length=200)
+    phonenumber = models.CharField('Номер владельца', max_length=20)
+    pure_phone = PhoneNumberField(
+        'Нормализированный номер владельца', blank=True, null=True)
+    flats = models.ManyToManyField(
+        Flat,
+        related_name='flat_owners',
+        verbose_name='Квартиры в собственности',
+        blank=True
+    )
+
+    def __str__(self):
+        return f'{self.full_name}'
