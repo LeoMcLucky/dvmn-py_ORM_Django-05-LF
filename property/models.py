144476338993c0217fc0536666c2163ad3710e23
Lev <leon.fissenko@gmail.com>
2025-10-23 18:54:01 +0500

    Edited models.py to pep8

--- property/models.py
+++ property/models.py
@@ -8,45 +8,73 @@ class Flat(models.Model):
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
-        db_index=True)
+        db_index=True
+    )
 
-    description = models.TextField('Текст объявления', blank=True)
-    price = models.IntegerField('Цена квартиры', db_index=True)
+    description = models.TextField(
+        'Текст объявления',
+        blank=True
+    )
+
+    price = models.IntegerField(
+        'Цена квартиры',
+        db_index=True
+    )
 
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
-        db_index=True)
+        db_index=True
+    )
+
     town_district = models.CharField(
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное'
+    )
+
     address = models.TextField(
         'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text='ул. Подольских курсантов д.5 кв.4'
+    )
+
     floor = models.CharField(
         'Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
+        help_text='Первый этаж, последний этаж, пятый этаж'
+    )
 
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
-        db_index=True)
+        db_index=True
+    )
+
     living_area = models.IntegerField(
         'количество жилых кв.метров',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True
+    )
 
     has_balcony = models.BooleanField(
-        'Наличие балкона', null=True, blank=True, db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+        'Наличие балкона',
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    active = models.BooleanField(
+        'Активно-ли объявление',
+        db_index=True
+    )
+
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True
+    )
 
     new_building = models.BooleanField(
         'Новостройка',
@@ -54,6 +82,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True
     )
+
     liked_by = models.ManyToManyField(
         User,
         related_name='liked_flats',
@@ -72,32 +101,42 @@ class Complaint(models.Model):
         related_name='complaints_made',
         verbose_name='Кто жаловался',
     )
+
     flat = models.ForeignKey(
         Flat,
         on_delete=models.CASCADE,
         related_name='flat_complaints',
         verbose_name='Квартира, на которую пожаловались'
-
     )
-    text = models.TextField('Техт жалобы', blank=True)
+
+    text = models.TextField(
+        'Техт жалобы',
+        blank=True
+    )
 
     def __str__(self):
         return f'{self.complainant}, {self.flat}, {self.text}'
 
 
 class Owner(models.Model):
-    full_name = models.CharField('ФИО владельца', max_length=200)
+    full_name = models.CharField(
+        'ФИО владельца',
+        max_length=200
+    )
+
     phonenumber = models.CharField(
         'Номер владельца',
         max_length=20,
         db_index=True
     )
+
     pure_phone = PhoneNumberField(
         'Нормализированный номер владельца',
         blank=True,
         null=True,
         db_index=True
     )
+
     flat = models.ManyToManyField(
         Flat,
         related_name='flat_owners',
